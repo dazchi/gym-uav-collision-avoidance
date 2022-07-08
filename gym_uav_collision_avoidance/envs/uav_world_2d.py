021c90d067fc538a271161a83e0e52506158a9f9
Darren Chi <crazypatoto@gmail.com>
2022-07-09 01:56:38 +0800

    Update to continuous action space

--- gym_uav_collision_avoidance/envs/uav_world_2d.py
+++ gym_uav_collision_avoidance/envs/uav_world_2d.py
@@ -5,36 +5,43 @@ import numpy as np
 
 
 class UAVWorld2D(gym.Env):
-    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
+    metadata = {"render_fps": 30}
 
-    def __init__(self, size=5):
-        self.size = size  # The size of the square grid
-        self.window_size = 512  # The size of the PyGame window
+    def __init__(self, x_size=100.0, y_size=100.0, agent_num=4, max_speed=10.0, max_acceleration=5.0):
+        self.x_size = x_size # size of x dimension
+        self.y_size = y_size # size of y dimension
+        self.min_location = np.array([-x_size/2.0, -y_size/2.0])
+        self.max_location = np.array([x_size/2.0, y_size/2.0])
+        self.max_speed = max_speed # Maximum speed of uav
+        self.max_acceleration = max_acceleration # Maximum acceleration of uav    
+        self.max_window_size = 800  # The size of the PyGame window
+        if x_size > y_size:
+            self.window_size_x = self.max_window_size
+            self.window_size_y = self.max_window_size / x_size * y_size            
+        else:
+            self.window_size_y = self.max_window_size
+            self.window_size_x = self.max_window_size / y_size * x_size        
+        
 
         # Observations are dictionaries with the agent's and the target's location.
         # Each location is encoded as an element of {0, ..., `size`}^2, i.e. MultiDiscrete([size, size]).
         self.observation_space = spaces.Dict(
             {
-                "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
-                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+                "agent": spaces.Box(low=self.min_location, high=self.max_location, dtype=np.float64),
+                "target": spaces.Box(low=self.min_location, high=self.max_location, dtype=np.float64),
             }
         )
 
         # We have 4 actions, corresponding to "right", "up", "left", "down", "right"
-        self.action_space = spaces.Discrete(4)
+        self.action_space = spaces.Dict(
+            {
+                "vel_x": spaces.Box(low=-max_speed, high=max_speed, shape=(1,), dtype=np.float64),
+                "vel_y": spaces.Box(low=-max_speed, high=max_speed, shape=(1,), dtype=np.float64),
+            }
+        )
+        # self.action_space = spaces.Box(-max_speed, max_speed, shape=(2,), dtype=np.float64)
 
-        """
-        The following dictionary maps abstract actions from `self.action_space` to 
-        the direction we will walk in if that action is taken.
-        I.e. 0 corresponds to "right", 1 to "up" etc.
-        """
-        self._action_to_direction = {
-            0: np.array([1, 0]),
-            1: np.array([0, 1]),
-            2: np.array([-1, 0]),
-            3: np.array([0, -1]),
-        }
-
+        
         """
         If human-rendering is used, `self.window` will be a reference
         to the window that we draw to. `self.clock` will be a clock that is used
@@ -55,28 +62,21 @@ class UAVWorld2D(gym.Env):
             )
         }
 
-    def reset(self, seed=None, return_info=False, options=None):
-        # We need the following line to seed self.np_random
-        super().reset(seed=seed)
-
-        # Choose the agent's location uniformly at random
-        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
-
-        # We will sample the target's location randomly until it does not coincide with the agent's location
-        self._target_location = self._agent_location
-        while np.array_equal(self._target_location, self._agent_location):
-            self._target_location = self.np_random.integers(0, self.size, size=2, dtype=int)
-
+    def reset(self, return_info=False, options=None):
+        # Choose the UoI's location uniformly at random
+        self._agent_location = np.random.uniform(self.min_location, high=self.max_location, size=(2,)).astype(np.float64)
+        
+        # Choose the goal's location uniformly at random
+        self._target_location = np.random.uniform(self.min_location, high=self.max_location, size=(2,)).astype(np.float64)        
         observation = self._get_obs()
         info = self._get_info()
         return (observation, info) if return_info else observation
 
-    def step(self, action):
-        # Map the action (element of {0,1,2,3}) to the direction we walk in
-        direction = self._action_to_direction[action]
+    def step(self, action):            
         # We use `np.clip` to make sure we don't leave the grid
+        # dt = self.clock.tick(60) * 0.001
         self._agent_location = np.clip(
-            self._agent_location + direction, 0, self.size - 1
+            self._agent_location + np.concatenate((action['vel_x'], action['vel_y']), axis=0), self.min_location , self.max_location
         )
         # An episode is done iff the agent has reached the target
         done = np.array_equal(self._agent_location, self._target_location)
@@ -86,67 +86,51 @@ class UAVWorld2D(gym.Env):
 
         return observation, reward, done, info
 
-    def render(self, mode="human"):
-        if self.window is None and mode == "human":
+    def render(self):
+        if self.window is None:
             pygame.init()
             pygame.display.init()
-            self.window = pygame.display.set_mode((self.window_size, self.window_size))
-        if self.clock is None and mode == "human":
+            self.window = pygame.display.set_mode((self.window_size_x, self.window_size_y))
+        if self.clock is None:
             self.clock = pygame.time.Clock()
 
-        canvas = pygame.Surface((self.window_size, self.window_size))
+        canvas = pygame.Surface((self.window_size_x, self.window_size_y))
         canvas.fill((255, 255, 255))
-        pix_square_size = (
-            self.window_size / self.size
-        )  # The size of a single grid square in pixels
-
+        pixel_per_meter = self.window_size_x / self.x_size
+        object_render_size = 10
+        
         # First we draw the target
+        target_render_location = (self._target_location + np.array([self.x_size/2, self.y_size/2])) * pixel_per_meter 
+        target_render_location[1] = self.window_size_y - target_render_location[1] 
         pygame.draw.rect(
             canvas,
             (255, 0, 0),
             pygame.Rect(
-                pix_square_size * self._target_location,
-                (pix_square_size, pix_square_size),
+                target_render_location,
+                (object_render_size, object_render_size),
             ),
         )
+        
         # Now we draw the agent
+        agent_render_location = (self._agent_location + np.array([self.x_size/2, self.y_size/2])) * pixel_per_meter 
+        agent_render_location[1] = self.window_size_y - agent_render_location[1] 
         pygame.draw.circle(
             canvas,
             (0, 0, 255),
-            (self._agent_location + 0.5) * pix_square_size,
-            pix_square_size / 3,
+            agent_render_location,
+            object_render_size,
         )
+        
 
-        # Finally, add some gridlines
-        for x in range(self.size + 1):
-            pygame.draw.line(
-                canvas,
-                0,
-                (0, pix_square_size * x),
-                (self.window_size, pix_square_size * x),
-                width=3,
-            )
-            pygame.draw.line(
-                canvas,
-                0,
-                (pix_square_size * x, 0),
-                (pix_square_size * x, self.window_size),
-                width=3,
-            )
+        # The following line copies our drawings from `canvas` to the visible window
+        self.window.blit(canvas, canvas.get_rect())
+        pygame.event.pump()
+        pygame.display.update()
 
-        if mode == "human":
-            # The following line copies our drawings from `canvas` to the visible window
-            self.window.blit(canvas, canvas.get_rect())
-            pygame.event.pump()
-            pygame.display.update()
-
-            # We need to ensure that human-rendering occurs at the predefined framerate.
-            # The following line will automatically add a delay to keep the framerate stable.
-            self.clock.tick(self.metadata["render_fps"])
-        else:  # rgb_array
-            return np.transpose(
-                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
-            )
+        # We need to ensure that human-rendering occurs at the predefined framerate.
+        # The following line will automatically add a delay to keep the framerate stable.
+        self.clock.tick(self.metadata["render_fps"])
+       
 
     def close(self):
         if self.window is not None:
--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 import gym
 from gym_uav_collision_avoidance.envs import UAVWorld2D
+import time
 
 env = UAVWorld2D()
-env.action_space.seed(100)
 
-observation, info = env.reset(seed=42, return_info=True)
+observation, info = env.reset(return_info=True)
 
-for _ in range(1000):
+while True:
     observation, reward, done, info = env.step(env.action_space.sample())
     env.render()
 
     if done:
         observation, info = env.reset(return_info=True)
+        
 
 env.close()
