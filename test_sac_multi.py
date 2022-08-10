21135ac8dde6210b135d739fe8dcbbe92adfda05
Darren Chi <crazypatoto@gmail.com>
2022-08-10 21:23:23 +0800

    Update test_sac_multi.py

--- test_sac_multi.py
+++ test_sac_multi.py
@@ -16,11 +16,12 @@ from torchviz import make_dot
 
 MODEL_PATH = './weights/sac_multi'
 WARM_UP_STEPS = 3000
-MAX_EPISOED_STEPS = 3000
-TOTAL_EPISODES = 20000
+MAX_EPISOED_STEPS = 1500
+TOTAL_EPISODES = 10000
 BATCH_SIZE = 256
-EVALUATE = False
 UPDATE_PER_STEP = 1
+EVALUATE_EPISODES = 10
+EVALUATE = False
 LOAD_MODEL = False
 NUM_AGENTS = 12
 EPSILON_GREEDY = 0.95
@@ -34,7 +35,8 @@ print('Using device = %s' % device)
 
 
 env = MultiUAVWorld2D(num_agents=NUM_AGENTS)
-tb_writer = SummaryWriter()
+comment = "%d_%d_%d_%d_%d_%d" % (env.x_size, env.max_speed[0], env.max_acceleratoin[0], NUM_AGENTS, env.collider_radius, env.d_sense)
+tb_writer = SummaryWriter(comment=comment)
 
 n_observations = env.observation_space.shape[0]
 n_actions = env.action_space.shape[0]
@@ -58,8 +60,9 @@ if EVALUATE or LOAD_MODEL:
 memory = ReplayMemory(int(1e6))
 updates = 0
 total_steps = 0
+
 states, _ = env.reset(return_info=True)
-for eps in range(TOTAL_EPISODES): 
+for eps in range(TOTAL_EPISODES):    
     score = 0
     eps_t = time.time()
     eps_steps = 0
@@ -114,28 +117,66 @@ for eps in range(TOTAL_EPISODES):
             # print(dones)
             if all(dones):
                 break            
-        
-    success_rate = env.target_reach_count / NUM_AGENTS
-    collision_rate = env.collision_count / NUM_AGENTS
-    
+                
     state, info = env.reset(return_info=True)
     eps_t = time.time() - eps_t
     steps_per_sec = eps_steps / eps_t
     sys.stdout.write("\033[K")
-    print("Total Steps = %d, Episode = %d, Score = %.3f, Steps Per Sec = %.2f, SR = %.2f, CR = %.2f" % (total_steps, eps, score, steps_per_sec, success_rate, collision_rate))    
+    print("Total Steps = %d, Episode = %d, Score = %.3f, Steps Per Sec = %.2f" % (total_steps, eps, score, steps_per_sec))    
     tb_writer.add_scalar("Score/Episodes", score, eps)
-    tb_writer.add_scalar("SR/Episodes", success_rate, eps)
-    tb_writer.add_scalar("CR/Episodes", collision_rate, eps)
-
+    tb_writer.flush()
     # print(torch.cuda.memory_summary())
 
     if not EVALUATE:
         agents[0].save_checkpoint(MODEL_PATH)
-   
 
-    tb_writer.flush()
+    # Eavluate model
+    if not EVALUATE and eps % EVALUATE_EPISODES == 0 and total_steps >= WARM_UP_STEPS:
+        print("-----------Start Evaluating-----------")
+        success_count = 0
+        collision_count = 0
+        for eva_eps in range(EVALUATE_EPISODES):
+            score = 0
+            eps_t = time.time()            
+            for eps_steps in range(MAX_EPISOED_STEPS):
+                actions = []
+                converted_actions = []
 
-    
+                for i in range(NUM_AGENTS):                
+                    action = agents[i].select_action(states[i], evaluate=True)        
+                    v = (action[0]/2+0.5) * np.linalg.norm(env.action_space.high)     
+                    theta = action[1] * math.pi                
+                    converted_action = np.array([v*math.cos(theta), v*math.sin(theta)])
+                    
+                    actions.append(action)
+                    converted_actions.append(converted_action)
+                                
+                next_states, rewards, dones, _ = env.step(converted_actions) # Step                                                                                                                                                                       
+                states = next_states
+                score += rewards[0]                 
+                print("EVA: Steps = %d, Reward = %.3f, Score = %.3f" % (steps, rewards[0], score), end='\r')        
+                env.render()
+
+                if all(dones):
+                    break
+            
+            success_count += env.target_reach_count
+            collision_count += env.collision_count     
+            state, info = env.reset(return_info=True)
+            eps_t = time.time() - eps_t
+            steps_per_sec = eps_steps / eps_t       
+            sys.stdout.write("\033[K")            
+            print("EVA: Total Steps = %d, Episode = %d-%d, Score = %.3f, Steps Per Sec = %.2f" % (total_steps, eps, eva_eps, score, steps_per_sec))    
+        
+        print(success_count)
+        print(collision_count)
+        success_rate = success_count / (NUM_AGENTS * EVALUATE_EPISODES)
+        collision_rate = collision_count / (NUM_AGENTS * EVALUATE_EPISODES)
+        print("SR = %.2f, CR = %.2f" % (success_rate, collision_rate)) 
+        tb_writer.add_scalar("SR/Episodes", success_rate, eps)
+        tb_writer.add_scalar("CR/Episodes", collision_rate, eps)
+        tb_writer.flush()
+        print("-----------End Evaluating-----------")
 
 env.close()
 tb_writer.close()
